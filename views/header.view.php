bc02053ff0ba1858d48de062803135d1b99bcf2b
DIEGO-DEVESOFT <diegogonzalezprogramador@gmail.com>
2024-02-25 20:04:26 -0500

    aguegue el icono en el header

--- views/header.view.php
+++ views/header.view.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/styles/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/img/imgCarrusel1.jpg">
 </head>
 
 <body>
